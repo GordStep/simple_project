6c755107a0479511197e253a51d09b912c5eae6d
4eker <113763398+4eker@users.noreply.github.com>
2025-04-13 17:58:38 +0300

    1.03
    перегрузка

--- MyMath.cpp
+++ MyMath.cpp
@@ -1,7 +1,6 @@
 #include "MyMath.h"
 
-template <typename T>
-T my_abs( T arg )
+double my_abs( double arg )
 {
   if (arg < 0)
     return -1 * arg;
@@ -9,12 +8,11 @@ T my_abs( T arg )
   return arg;
 }
 
-template <typename T>
-T my_n_pow(T arg, int pow)
+double my_n_pow(double arg, int pow)
 {
-  if (pow == 0) return T(1);
+  if (pow == 0) return 1;
 
-  T res = 1;
+  double res = 1;
 
   if (pow > 0)
   {
@@ -32,4 +30,68 @@ T my_n_pow(T arg, int pow)
   }
 
   return res;
+}
+
+float my_abs(float arg)
+{
+    if (arg < 0)
+        return -1 * arg;
+
+    return arg;
+}
+
+float my_n_pow(float arg, int pow)
+{
+    if (pow == 0) return 1;
+
+    float res = 1;
+
+    if (pow > 0)
+    {
+        for (int i = 0; i < pow; i++)
+        {
+            res *= arg;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < -1 * pow; i++)
+        {
+            res /= arg;
+        }
+    }
+
+    return res;
+}
+
+int my_abs(int arg)
+{
+    if (arg < 0)
+        return -1 * arg;
+
+    return arg;
+}
+
+int my_n_pow(int arg, int pow)
+{
+    if (pow == 0) return 1;
+
+    int res = 1;
+
+    if (pow > 0)
+    {
+        for (int i = 0; i < pow; i++)
+        {
+            res *= arg;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < -1 * pow; i++)
+        {
+            res /= arg;
+        }
+    }
+
+    return res;
 }
